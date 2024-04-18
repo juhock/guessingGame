11e7b3cc34e4fcbf176fb6bbaa0bc920aed093c6
Julia Hocken <ju.hocken@gmail.com>
2024-04-18 15:26:51 -0400

    logic for highscore added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
 document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -9,9 +13,15 @@ document.querySelector(".check").addEventListener("click", function () {
       "⛔️ Please enter a number.";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct!";
+    highScore++;
+    document.querySelector(".highscore").textContent = highScore;
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "Too high!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
