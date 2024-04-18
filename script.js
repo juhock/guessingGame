4d18231775a600f9b168792e3da9820a60e884c3
Julia Hocken <ju.hocken@gmail.com>
2024-04-18 15:02:52 -0400

    added event listener for guess and logic for no guess

---
+++ script.js
@@ -0,0 +1,10 @@
+const number = Math.random();
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "Please enter a number.";
+  }
+});
