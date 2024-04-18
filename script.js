591f65b8147bfcae13d5e8e3af225f0376990e70
Julia Hocken <ju.hocken@gmail.com>
2024-04-18 15:15:21 -0400

    added logic for correct number guessed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const number = Math.random();
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "Please enter a number.";
+    document.querySelector(".message").textContent =
+      "⛔️ Please enter a number.";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct!";
   }
 });
