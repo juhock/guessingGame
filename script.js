fcdce93bbda07fcdefe63947c6c7e5d08879ed67
Julia Hocken <ju.hocken@gmail.com>
2024-04-20 20:20:15 -0400

    display message refactoring

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   //No guess
   if (!guess) {
-    document.querySelector(".message").textContent =
-      "⛔️ Please enter a number.";
+    displayMessage("⛔️ Please enter a number.");
 
     //Correct guess & highscore
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct!";
+    displayMessage("🎉 Correct!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
@@ -27,12 +30,11 @@ document.querySelector(".check").addEventListener("click", function () {
 
     //wrong guess
   } else if (guess !== secretNumber) {
-    document.querySelector(".message").textContent =
-      guess > secretNumber ? "Too high!" : "Too low!";
+    displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
     score--;
     document.querySelector(".score").textContent = score;
   } else {
-    document.querySelector(".message").textContent = "Game Over";
+    displayMessage("Game Over");
     document.querySelector(".score").textContent = 0;
     document.querySelector("body").style.backgroundColor = "#AA0000";
     document.querySelector(".number").textContent = secretNumber;
@@ -44,9 +46,9 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
-  document.querySelector(".number").textContent = "?";
+  displayMessage("?");
   document.querySelector(".guess").value = "";
 
   document.querySelector("body").style.backgroundColor = "#222";
